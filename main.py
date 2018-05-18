72217f45ebd94c829844c85da6a6fa9832723a7c
Arjun Menon <arjunravimenon@gmail.com>
2018-05-18 10:05:25 +0000

    working Tensorflow example

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
 from IPython import display
 from matplotlib import cm
-from matplotlib import gridspec
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
@@ -10,7 +9,7 @@ import tensorflow as tf
 pd.options.display.max_rows = 10
 pd.options.display.float_format = '{:.1f}'.format
 
-california_housing_dataframe = pd.read_csv("https://storage.googleapis.com/mledu-datasets/california_housing_train.csv", sep=",")
+california_housing_dataframe = pd.read_csv("http://storage.googleapis.com/mledu-datasets/california_housing_train.csv", sep=",")
 
 california_housing_dataframe = california_housing_dataframe.reindex(
     np.random.permutation(california_housing_dataframe.index))
